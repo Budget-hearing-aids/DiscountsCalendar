36125bdf420ec5d4b21cb40adc049f9869d09cc3
Budget-hearing-aids <ahmed@audienhearing.com>
2024-10-06 08:40:22 +0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,22 @@
+// Discount data
+const discountData = {
+    "October": [
+        { start: "2024-10-08", end: "2024-10-09", description: "Ion Series $150 off prime day (2 emails 1 text each day)" },
+        { start: "2024-10-31", end: "2024-10-31", description: "Halloween sale (Atom 2 Series) - 2 emails 1 text" }
+    ],
+    "November": [
+        { start: "2024-11-15", end: "2024-11-20", description: "$50 off 2 Series" },
+        { start: "2024-11-15", end: "2024-11-20", description: "25% off Ion Series" },
+        { start: "2024-11-21", end: "2024-11-24", description: "20% off all products" },
+        { start: "2024-11-25", end: "2024-11-28", description: "$125 off Ion Series" },
+        { start: "2024-11-29", end: "2024-12-01", description: "BOGO Atom 2 Series" },
+        { start: "2024-11-29", end: "2024-12-01", description: "$150 off Ion Series" }
+    ],
+    "December": [
+        { start: "2024-12-02", end: "2024-12-04", description: "Final Sale: 20% off all products - Cyber Monday" },
+        { start: "2024-12-05", end: "2024-12-09", description: "$75 off Atom 2 Series" },
+        { start: "2024-12-10", end: "2024-12-12", description: "30% off Ion Series" },
+        { start: "2024-12-13", end: "2024-12-17", description: "BOGO All Products" },
+        { start: "2024-12-24", end: "2024-12-25", description: "$150 off Ion Series, $80 off Atom 2 Series" }
+    ]
+};
