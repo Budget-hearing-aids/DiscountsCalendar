4d9e13f2411bee47793341e94d31d3389a2ab071
Budget-hearing-aids <ahmed@audienhearing.com>
2024-11-29 20:11:39 +0200

    Update data.js

--- data.js
+++ data.js
@@ -13,9 +13,17 @@ const discountData = {
     ],
     "December": [
         { start: "2024-12-02", end: "2024-12-04", description: "BOGO atom 2 series ($100 off ion series)" },
-        { start: "2024-12-05", end: "2024-12-09", description: "$75 off Atom 2 Series" },
-        { start: "2024-12-10", end: "2024-12-12", description: "30% off Ion Series" },
-        { start: "2024-12-13", end: "2024-12-17", description: "BOGO All Products" },
-        { start: "2024-12-24", end: "2024-12-25", description: "$150 off Ion Series, $80 off Atom 2 Series" }
-    ]
+        { start: "2024-12-05", end: "2024-12-05", description: "$50 off Atom 2 Series" },
+        { start: "2024-12-06", end: "2024-12-06", description: "$125 off ION only" },
+        { start: "2024-12-07", end: "2024-12-07", description: "20% off atom 2 series" },
+        { start: "2024-12-08", end: "2024-12-08", description: "$100 off atom pro 2" },
+        { start: "2024-12-09", end: "2024-12-09", description: "BOGO ion series" },
+        { start: "2024-12-10", end: "2024-12-10", description: "$80 off atom pro 2" },
+        { start: "2024-12-11", end: "2024-12-11", description: "30% off atom 2 series" },
+        { start: "2024-12-12", end: "2024-12-12", description: "$150 off ion series" },
+        { start: "2024-12-13", end: "2024-12-13", description: "$150 off ion pro" },
+        { start: "2024-12-14", end: "2024-12-14", description: "25% off ion series" },
+        { start: "2024-12-15", end: "2024-12-15", description: "20% off all products" },
+        { start: "2024-12-16", end: "2024-12-16", description: "BOGO ion series" },
+            ]
 };
